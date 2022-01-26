ebe15c9cf32d418fbaee327d356111cf6a47b8c2
newtonrodrigues <newton.nw100@gmail.com>
2022-01-26 20:25:09 -0300

    javascript number

---
+++ javascript-slider/javascript.js
@@ -0,0 +1,5 @@
+function ajustar(){
+  let size = document.getElementById("slider").value;
+  console.log(document.getElementById("slider").value);
+  document.getElementById("imagem").style=`width: ${size}%; height: ${size}%;` 
+}
